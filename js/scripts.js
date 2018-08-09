df4454dbd0199984c911c609b9eb56c116a58dec
Chris Crow and Victoria Oh <victoria@>
2018-08-09 15:11:57 -0700

    Add initial files. Add html and variables for all sides of traingle.

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $("#submit").click(event(){
+
+  var side1 = parseInt($("#side1").val());
+  var side2 = parseInt($("#side2").val());
+  var side3 = parseInt($("#side3").val());
+
+
+
+
+
+
+
+
+
+
+
+  });
+
+});
